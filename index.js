0bd46b886171d0f2321620e9c4fbe9765c679b74
MabohlaleNkuna <111710433+MabohlaleNkuna@users.noreply.github.com>
2025-02-03 11:51:50 +0200

    upd

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -8,21 +7,16 @@ const recipeRoutes = require('./routes/recipeRoutes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json()); 
 
-// Database connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected successfully'))
+  .then(() => console.log('Connected to MongoDB Atlas successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// Define your recipe routes
 app.use('/', recipeRoutes); 
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server is running');
+});  
